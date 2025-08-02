5551d408b1e923cf411f6b0956c7855366b35f2c
Danilo Dias <dgd1.dev@gmail.com>
2025-08-01 21:20:52 -0300

    First commit!

---
+++ src/main/kotlin/br/com/dgdlabs/frifasrifas/FrIFasRifasApplication.kt
@@ -0,0 +1,11 @@
+package br.com.dgdlabs.frifasrifas
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FrIFasRifasApplication
+
+fun main(args: Array<String>) {
+	runApplication<FrIFasRifasApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/dgdlabs/frifasrifas/FrIFasRifasApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.dgdlabs.frifasrifas
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FrIFasRifasApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
